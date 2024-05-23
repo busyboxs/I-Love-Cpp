0477f1ca94e0b89d3556b11797b2ff40326a1c13
busyboxs <1823518889@qq.com>
2024-05-23 17:48:52 +0800

    feat: 添加 windows 下 wstring 和 string 的相互转换

--- helper/string_helper.h
+++ helper/string_helper.h
@@ -3,6 +3,50 @@
 #include <string>
 #include <vector>
 
+#ifdef WIN32
+#define NOMINMAX
+#include <Windows.h>
+namespace string_helper
+{
+	inline std::string ws2s(const std::wstring& wide, UINT CodePage = CP_ACP)
+	{
+		int wide_length = static_cast<int>(wide.length());
+		if (wide_length == 0)
+			return std::string();
+
+		int charcount = WideCharToMultiByte(CodePage, 0, wide.data(), wide_length, NULL, 0, NULL, NULL);
+		if (charcount == 0)
+			return std::string();
+
+		std::string mb;
+		mb.resize(charcount);
+		WideCharToMultiByte(CodePage, 0, wide.data(), wide_length, &mb[0], charcount, NULL, NULL);
+
+		return mb;
+	}
+
+	inline std::wstring s2ws(const std::string& mb, UINT CodePage = CP_ACP)
+	{
+		if (mb.empty())
+			return std::wstring();
+
+		int mb_length = static_cast<int>(mb.length());
+
+		int charcount = MultiByteToWideChar(CodePage, 0, mb.data(), mb_length, NULL, 0);
+		if (charcount == 0)
+			return std::wstring();
+
+		std::wstring wide;
+		wide.resize(charcount);
+		MultiByteToWideChar(CodePage, 0, mb.data(), mb_length, &wide[0], charcount);
+
+		return wide;
+	}
+}
+
+#endif // WIN32
+
+
 namespace string_helper
 {
 	/**
@@ -17,12 +61,41 @@ namespace string_helper
 	}
 
 	/**
+	 * @brief 移除字符串中的空格，并返回修改后的字符串
+	 * @param str 要被移除空格，换行符等符号（‘ ’，‘\f’, '\n', '\r', '\t', '\v'）的字符串
+	 * @return 修改后的字符串
+	 */
+	inline std::wstring RemoveSpace(std::wstring str)
+	{
+		str.erase(std::remove_if(str.begin(), str.end(), ::iswspace), str.end());
+		return str;
+	}
+
+	/**
+	 * @brief 替换字符串中旧字符串为新字符串
+	 * @param str 待替换的字符串
+	 * @param old_str 要被替换的旧字符串
+	 * @param new_str 要替换的新字符串
+	 * @return 修改后的字符串
+	 */
+	inline std::string ReplaceStr(std::string str, const std::string& old_str, const std::string& new_str)
+	{
+		size_t pos = 0;
+		while ((pos = str.find(old_str, pos)) != std::string::npos)
+		{
+			str.replace(pos, old_str.length(), new_str);
+			pos += new_str.length();
+		}
+		return str;
+	}
+
+	/**
 	 * @brief 将字符串按分隔符进行分割，分割结果保存到 vector 中
 	 * @param [in] s 输入字符串
 	 * @param [out] tokens 分割结果列表
 	 * @param delimiters 分割符
 	*/
-	inline void split(const std::string& s, std::vector<std::string>& tokens, const std::string& delimiters = ";")
+	inline void Split(const std::string& s, std::vector<std::string>& tokens, const std::string& delimiters = ";")
 	{
 		tokens.clear();
 		auto lastPos = s.find_first_not_of(delimiters, 0);
@@ -42,7 +115,7 @@ namespace string_helper
 	 * @param skip_empty 是否跳过空字符串，若跳过，则返回结果中不包括空
 	 * @return 分割结果列表
 	*/
-	inline std::vector<std::string> split(const std::string& str, const std::string& delimiter = ";", bool skip_empty = true)
+	inline std::vector<std::string> Split(const std::string& str, const std::string& delimiter = ";", bool skip_empty = true)
 	{
 		if (str.empty())
 		{
@@ -80,7 +153,7 @@ namespace string_helper
 	 * @param delimiter 拼接符
 	 * @return 返回拼接后的字符串
 	*/
-	inline std::string join(const std::vector<std::string>& inputs, const std::string& delimiter = ";")
+	inline std::string Join(const std::vector<std::string>& inputs, const std::string& delimiter = ";")
 	{
 		if (inputs.empty())
 			return std::string();
